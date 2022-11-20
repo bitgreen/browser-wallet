2e521b4f561b944732a7f1805f90ceaee9fca355
rappix <rappix95@gmail.com>
2022-11-21 00:08:39 +0100

    remove old build scripts

--- setup-chrome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp extension/manifest-chrome.json extension/manifest.json
-echo "Setup for Chrome/Edge/Brave completed"
--- setup-firefox.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp extension/manifest-firefox.json extension/manifest.json
-echo "Setup for Firefox completed"
