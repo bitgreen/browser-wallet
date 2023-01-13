fe87f3f981eb455130b9ccdcbe1fd11c45482ca1
XeZZoR <dennis@bitgreen.org>
2023-01-13 15:19:24 +0100

    Added primitive build script

---
+++ buildDistribution.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p dist && rm dist/*.zip
+npm run build
+cd build/chrome/
+zip -r ../../dist/chrome.zip .
+cd ../firefox/
+zip -r ../../dist/firefox.zip .
+cd ../safari/
+zip -r ../../dist/safari.zip .
+
+## TODO Create a zip file of relevant source.
+
