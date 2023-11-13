7066410640dc1d8d425360e96c04d65529032d55
rappix <rappix95@gmail.com>
2023-11-13 15:07:09 +0100

    fix regex

--- packages/browser-wallet-core/src/app.js
+++ packages/browser-wallet-core/src/app.js
@@ -1,4 +1,5 @@
 import { knownApps } from "./constants.js";
+import escapeStringRegexp from 'escape-string-regexp';
 
 export const checkIfAppIsKnown = (domain) => {
     // Check for exact match first
@@ -10,9 +11,12 @@ export const checkIfAppIsKnown = (domain) => {
 
     // Check for wildcard domains
     for(const app of knownApps) {
-        // Create a regex pattern from the domain, replacing '*' with a regex wildcard
-        const pattern = app.domain.replace(/\./g, '\\.').replace(/\*/g, '.*')
+        // Escape regex characters and replace '*' with a regex wildcard
+        const pattern = escapeStringRegexp(app.domain.replace(/\*/g, '__WILDCARD__')).replace(/__WILDCARD__/g, '.*')
         const regex = new RegExp(`^${pattern}$`);
+
+        console.log(pattern)
+        console.log(domain)
 
         if (regex.test(domain)) {
             return app;
