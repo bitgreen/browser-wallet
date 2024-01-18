3f0fbd7118be831f07fd92382886482e36176275
rappix <rappix95@gmail.com>
2024-01-18 04:34:56 +0100

    transaction updates

--- packages/browser-wallet-core/src/stores/asset.js
+++ packages/browser-wallet-core/src/stores/asset.js
@@ -18,13 +18,13 @@ class AssetStore extends BaseStore {
   async fetch() {
     if(!['mainnet', 'testnet'].includes(this.network.id)) return false;
 
-    const url = this.network.api_endpoint + '/asset/transactions?account=' + this.account.address;
+    const url = this.network.api_endpoint + '/asset/transactions?account=' + this.account.address + '&pageSize=10&page=1';
     let result = await fetch(url, {
       mode: 'cors'
     })
     result = await result.json()
 
-    for(const asset of result) {
+    for(const asset of result.transactions) {
       await this.asyncSet(asset.id, asset)
     }
 
