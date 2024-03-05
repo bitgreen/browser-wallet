7ba470bc73d3de337bc10d229b528f60c8e9e0b7
Mike Burns <burns@bitgreen.org>
2024-03-05 12:23:17 +0100

    force bash for icon generation

---
+++ packages/browser-wallet-base/generate-icons.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --target*)
+      if [[ $key == "--target="* ]]; then
+        target="${key#*=}"
+      else
+        target="$2"
+        shift
+      fi
+      ;;
+    *)
+      echo "Unknown option: $key"
+      exit 1
+      ;;
+  esac
+  shift
+done
+npx capacitor-assets generate --iconBackgroundColor \#224851 --iconBackgroundColorDark \#224851 --splashBackgroundColor \#224851 --splashBackgroundColorDark \#224851 --${target} --assetPath src/app-resources --${target}Project ../../build/${target}
